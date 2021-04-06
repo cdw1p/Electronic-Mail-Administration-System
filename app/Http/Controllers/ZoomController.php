9fce818a9efafe3831f3f94342a3809ffd5654a7
System Administrator <cahyorizqullah@gmail.com>
2021-04-06 13:56:35 +0700

    Added: route api zoom (getMeeting)

--- app/Http/Controllers/ZoomController.php
+++ app/Http/Controllers/ZoomController.php
@@ -5,10 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use \MacsiDigital\Zoom\Facades\Zoom;
+use Carbon\Carbon;
 
 class ZoomController extends Controller
 {
   public function getUser() {
-    return Zoom::user()->all();
+    return Zoom::user()->first();
+  }
+
+  public function getMeeting() {
+    return Zoom::user()->first()->meetings()->get();
   }
 }
--- routes/api.php
+++ routes/api.php
@@ -15,3 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/zoom/getUser', 'ZoomController@getUser')->name('zoom.getUser');
+Route::get('/zoom/getMeeting', 'ZoomController@getMeeting')->name('zoom.getMeeting');
