231167ca03ad0978ce73ccf5739bca180c11a77a
Cahyo Dwi Putro <cahyorizqullah@gmail.com>
2021-06-14 15:29:35 +0700

    chore(update): sidebar statement

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -7,13 +7,21 @@
 
           @if (Auth::user()->role === 'admin')
             <li class="menu-title"><span>Modul Meeting</span></li>
-            <li {{ request()->routeIs('meeting.schedule.index') ? 'class=active' : '' }}><a href="{{ route('meeting.schedule.index') }}"><i class="fa fa-calendar"></i> <span>Atur Jadwal</span></a></li>
-            <li><a href="{{ route('meeting.invitation.index') }}"><i class="fa fa-user-plus"></i> <span>Undangan</span></a></li>
+            <li {{ (request()->routeIs('meeting.schedule.index') || request()->routeIs('meeting.schedule.create') || request()->routeIs('meeting.schedule.edit')) ? 'class=active' : '' }}>
+              <a href="{{ route('meeting.schedule.index') }}"><i class="fa fa-calendar"></i> <span>Atur Jadwal</span></a>
+            </li>
+            <li {{ (request()->routeIs('meeting.invitation.index') || request()->routeIs('meeting.invitation.create') || request()->routeIs('meeting.invitation.edit')) ? 'class=active' : '' }}>
+              <a href="{{ route('meeting.invitation.index') }}"><i class="fa fa-user-plus"></i> <span>Undangan</span></a>
+            </li>
             <li><a href="#"><i class="fa fa-clock"></i> <span>Presensi</span></a></li>
 
             <li class="menu-title"><span>Pengaturan</span></li>
-            <li {{ request()->routeIs('settings.users.index') ? 'class=active' : '' }}><a href="{{ route('settings.users.index') }}"><i class="fa fa-users"></i> <span>Master Pengguna</span></a></li>
-            <li {{ request()->routeIs('settings.app.index') ? 'class=active' : '' }}><a href="{{ route('settings.app.index') }}"><i class="fa fa-cog"></i> <span>Master Aplikasi</span></a></li>
+            <li {{ (request()->routeIs('settings.users.index') || request()->routeIs('settings.users.create') || request()->routeIs('settings.users.edit')) ? 'class=active' : '' }}>
+              <a href="{{ route('settings.users.index') }}"><i class="fa fa-users"></i> <span>Master Pengguna</span></a>
+            </li>
+            <li {{ request()->routeIs('settings.app.index') ? 'class=active' : '' }}>
+              <a href="{{ route('settings.app.index') }}"><i class="fa fa-cog"></i> <span>Master Aplikasi</span></a>
+            </li>
           @endif
         </ul>
       </div>
