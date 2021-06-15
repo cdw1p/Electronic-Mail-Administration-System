e8e79f564e6379c8e18c3e701ac5e81aa3910e61
Cahyo Dwi Putro <cahyorizqullah@gmail.com>
2021-06-15 09:17:28 +0700

    chore(update): multiple room statement

--- app/Http/Controllers/MeetingScheduleController.php
+++ app/Http/Controllers/MeetingScheduleController.php
@@ -29,13 +29,25 @@ class MeetingScheduleController extends Controller
 
   public function store(Request $request) {
     $findDatabase = Room::where('name', $request->name)->first();
-    $findZoomData = Zoom::user()->first()->meetings()->where('topic', $request->name)->get();
-    if (!$findDatabase && count($findZoomData) < 1) {
+    $findZoomData = Zoom::user()->first()->meetings()->all();
+    $matchZoomData = collect([]);
+    foreach ($findZoomData as $data) {
+      if ($data->topic === $request->name) {
+        $matchZoomData = $data;
+      }
+    }
+    if (!$findDatabase && count($matchZoomData) < 1) {
       $meeting = Zoom::meeting()->make([ 'topic' => $request->name, 'type' => 8, 'start_time' => new Carbon($request->start_date), 'duration' => $request->is_duration, 'timezone' => 'Asia/Jakarta' ]);
       $meeting->recurrence()->make([ 'type' => 3, 'repeat_interval' => '0', 'weekly_days' => '0', 'end_times' => '0' ]);
       $meeting->settings()->make([ 'join_before_host' => false, 'approval_type' => 0, 'registration_type' => 2, 'enforce_login' => false, 'auto_recording' => $request->is_recording ? 'cloud' : 'local' ]);
       $createMeeting = Zoom::user()->first()->meetings()->save($meeting);
-      $zoonData = Zoom::user()->first()->meetings()->where('topic', $request->name)->get();
+      $findLastestZoomData = Zoom::user()->first()->meetings()->all();
+      $zoomData = collect([]);
+      foreach ($findLastestZoomData as $data) {
+        if ($data->topic === $request->name) {
+          $zoomData = $data;
+        }
+      }
       $createRoom = Room::create([
         'name'                => $request->name,
         'start_date'          => new Carbon($request->start_date),
@@ -43,8 +55,8 @@ class MeetingScheduleController extends Controller
         'is_recording'        => $request->is_recording,
         'is_start_meeting'    => $request->is_start_meeting,
         'is_join_before_host' => false,
-        'zoom_id'             => $zoonData[0]->id,
-        'zoom_link'           => $zoonData[0]->join_url
+        'zoom_id'             => $zoomData->id,
+        'zoom_link'           => $zoomData->join_url
       ]);
       if ($createMeeting && $createRoom) {
         Session::flash('success', 'Selamat, data meeting berhasil disimpan kedalam database server!');
